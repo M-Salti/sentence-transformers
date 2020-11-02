72d37d5208e318fffa11f4321d13df5921cc3bb4
Mohamed Al Salti <mohamed.al.salti.96@gmail.com>
2020-11-03 00:39:28 +0200

    optimize only unfreezed params

--- sentence_transformers/SentenceTransformer.py
+++ sentence_transformers/SentenceTransformer.py
@@ -527,8 +527,8 @@ class SentenceTransformer(nn.Sequential):
 
             no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
             optimizer_grouped_parameters = [
-                {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
-                {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+                {'params': [p for n, p in param_optimizer if p.requires_grad and not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+                {'params': [p for n, p in param_optimizer if p.requires_grad and any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
             ]
 
             optimizer = optimizer_class(optimizer_grouped_parameters, **optimizer_params)
