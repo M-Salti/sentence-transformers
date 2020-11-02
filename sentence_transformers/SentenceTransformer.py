69322ce7b187fffbf5101a2c1aff87327e3ecad6
Mohamed Al Salti <mohamed.al.salti.96@gmail.com>
2020-11-03 01:12:46 +0200

    Update filtering

--- sentence_transformers/SentenceTransformer.py
+++ sentence_transformers/SentenceTransformer.py
@@ -523,12 +523,13 @@ class SentenceTransformer(nn.Sequential):
         optimizers = []
         schedulers = []
         for loss_model in loss_models:
-            param_optimizer = list(loss_model.named_parameters())
+            # param_optimizer = list(loss_model.named_parameters())
+            param_optimizer = list(filter(lambda named_param: named_param[1].requires_grad, loss_model.named_parameters()))
 
             no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
             optimizer_grouped_parameters = [
-                {'params': [p for n, p in param_optimizer if p.requires_grad and not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
-                {'params': [p for n, p in param_optimizer if p.requires_grad and any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+                {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+                {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
             ]
 
             optimizer = optimizer_class(optimizer_grouped_parameters, **optimizer_params)
